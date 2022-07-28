1575f139126f0fe2e7d90f434f06e16574c1f100
Armando Villanueva <armandojvilla9@gmail.com>
2022-07-28 07:32:54 -0400

    functionality created

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ExchangeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(ExchangeController::class)->group(function () {
+    Route::get('/exchange/{amount}/{from}/{to}', 'exchange');
 });
